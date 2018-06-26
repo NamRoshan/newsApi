c0d7e25200b559ead53646ce2660b994e7e7bf5d
NamRoshan <deubaroshan66@gmail.com>
2018-06-26 09:17:13 +0545

    first commit

---
+++ app/src/main/java/com/example/roshan/loginform/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.roshan.loginform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class LoginActivity extends AppCompatActivity {
+//    private  void showLog(String text){
+//        Log.d("hellow",text);
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+//        showLog("Activity created");
+    }
+}
